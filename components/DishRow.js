89fa7929439fd713445dff0faa6a97d65e5f362f
Umang Patel <umangpatel3105@gmail.com>
2023-05-24 00:11:33 +0530

    fea:ui changes

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,20 +1,31 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import Currency from "react-currency-formatter";
-
+import { urlFor } from "../sanity";
 const DishRow = ({ id, name, description, price, image }) => {
-  console.log("moneyyy", price);
   return (
-    <TouchableOpacity>
-      <View>
+    <TouchableOpacity classname="bg-white border p-4 border-gray-200">
+      <View classname="flex-row" >
         <Text classname="text-lg mb-1">{name}</Text>
         <Text classname="text-lg mb-1">{description}</Text>
         <Text classname="text-gray-400 mt-2">
-          <Currency
-            quantity={price} currency="INR"
-          />
+          <Currency quantity={price} currency="INR" />
         </Text>
       </View>
+      <View>
+        <Image
+          style={{
+            height: 96,
+            width: 96,
+            borderWidth: 1,
+            borderColor: "#F3F3F4",
+            padding:12,
+            // backgroundColor:"gray.300" 
+          }}
+          source={{ uri: urlFor(image).url() }}
+          classname=""
+        />
+      </View>
+      
     </TouchableOpacity>
   );
 };
