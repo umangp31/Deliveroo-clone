fc558864af8d32425ddb30a146d34cd0a1b0c72e
Umang Patel <umangpatel3105@gmail.com>
2023-05-24 15:05:56 +0530

    fixed ui

--- components/DishRow.js
+++ components/DishRow.js
@@ -3,29 +3,28 @@ import Currency from "react-currency-formatter";
 import { urlFor } from "../sanity";
 const DishRow = ({ id, name, description, price, image }) => {
   return (
-    <TouchableOpacity classname="bg-white border p-4 border-gray-200">
-      <View classname="flex-row" >
-        <Text classname="text-lg mb-1">{name}</Text>
-        <Text classname="text-lg mb-1">{description}</Text>
-        <Text classname="text-gray-400 mt-2">
-          <Currency quantity={price} currency="INR" />
-        </Text>
+    <TouchableOpacity className="bg-white border p-4 border-gray-200">
+      <View className="flex-row items-center ">
+        <View className="flex-1 pr-2 item ">
+          <Text className="text-lg mb-1">{name}</Text>
+          <Text className="text-gray-400">{description}</Text>
+          <Text className="text-gray-400 mt-2">
+            <Currency quantity={price} currency="INR" />
+          </Text>
+        </View>
+        <View>
+          <Image
+            style={{
+              borderWidth: 1,
+              borderColor: "#F3F3F4",
+              padding: 12,
+              // backgroundColor:"gray.300"
+            }}
+            source={{ uri: urlFor(image).url() }}
+            className="h-20 w-20 bg-gray-200 p-4 rounded-sm"
+          />
+        </View>
       </View>
-      <View>
-        <Image
-          style={{
-            height: 96,
-            width: 96,
-            borderWidth: 1,
-            borderColor: "#F3F3F4",
-            padding:12,
-            // backgroundColor:"gray.300" 
-          }}
-          source={{ uri: urlFor(image).url() }}
-          classname=""
-        />
-      </View>
-      
     </TouchableOpacity>
   );
 };
